60e58ce62ede7315e4ff63a024214e8c6480253a
Ningyu-Zhang98 <108341745+Ningyu-Zhang98@users.noreply.github.com>
2022-08-20 15:39:22 +0800

    tacoCat functions done
    tacoCat functions done

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
-//Call hello world
-function HelloWorld(){
-    alert('hello world');
+//Controller
+function getValues(){
+    //get tht values from the input
+    document.getElementById("alert").classList.add("invisible")
+    let inputText = document.getElementById("input").value;
+    inputText = inputText.toLowerCase()
+    //Input clean up
+    let regex = /[^a-z0-9]/gi;
+    inputText = inputText.replace(regex,"")
+
+    //Check if it's a palindrome
+    let res = checkPilindrome(inputText)
+    //Display 
+    disPlayPalindrome(res, inputText)
+}
+
+
+function checkPilindrome(input) {
+    let i = 0
+    let j = input.length - 1
+
+    while (i<j){
+        if (input[i] != input[j]){
+            return false
+        }
+        i += 1
+        j-=1
+    }
+    return true
+}
+
+function disPlayPalindrome(res,input) {
+    if (res){
+        document.getElementById("alertHeader").innerHTML = `Well Done!`
+        document.getElementById("msg").innerHTML = `Your message in a palindrome: ${input}`
+        document.getElementById("alert").classList.remove("invisible")
+    }else {
+        document.getElementById("alertHeader").innerHTML = `Sorry`
+        document.getElementById("msg").innerHTML = `Your message in NOT a palindrome: ${input}`
+        document.getElementById("alert").classList.remove("invisible")
+
+    }
+
 }
